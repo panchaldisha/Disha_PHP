fbb77f9c4ac824188c397506d2652f27a656839e
disha_php <panchalrameshbhai81@gmail.com>
2022-07-25 22:34:48 +0530

    updated

---
+++ extra/car.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title>Car Detail</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<div class="container" style="background-color=dark">
+<h1 align="center">Car details</h1>
+	
+
+<form action="" method="post">
+<fieldset>
+<table  align="center" class="table table-striped">
+
+<tr>
+            
+			<td > Name:</td>
+			<td><input type="text" name="name"class="form-control" ></td>
+    </tr> 
+	
+	
+	 <tr>
+          <td >Description:</td>
+          <td><input textarea="des" name="des" class="form-control"></td>
+		  
+	</tr>	
+	
+	
+	 <tr>
+          <td >Price:</td>
+          <td><input textarea="price" name="Price" class="form-control"></td>
+		  
+	</tr>	
+	
+	 <tr>
+          <td >Img:</td>
+          <td><input textarea="Img" name="Img" class="form-control"></td>
+		  
+	</tr>
+	
+	 <tr>
+          <td >Type:</td>
+          <td><input textarea="Type" name="Type" class="form-control"></td>
+		  
+	</tr>	
+	
+	 <tr>
+          <td >Fuel_type:</td>
+          <td><input textarea="Fuel_type" name="Fuel_type" class="form-control"></td>
+		  
+	</tr>	
+	
+	
+	
+	<tr> 
+          <td colspan="2"><input type="submit" name="submit" class="btn btn-primary"></td>
+		  
+          <td colspan="2"><input type="reset" name="reset" class="btn btn-primary"></td>
+	</tr>	  	   
+
+</div>
+</body>
+</head>
+</html>
